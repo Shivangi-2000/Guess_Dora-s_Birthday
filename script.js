56623b7326fd9215f81ddab4fb45b4406d1bf8ea
Shivangi Gupta <71278498+Shivangi-2000@users.noreply.github.com>
2023-01-16 14:43:23 +0530

    final change

--- script.js
+++ script.js
@@ -1,39 +1,62 @@
 'use strict';
 
-const birthDate = Math.trunc(Math.random() * 31 + 1);
+let birthDate = Math.trunc(Math.random() * 31 + 1);
 console.log(birthDate);
 let score = 20;
+let highScore = 0;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (score > 1) {
     if (!guess) {
       document.querySelector('.message').textContent = '⛔ No Value Entered!';
+      //if you win the game
     } else if (guess === birthDate) {
+      document.querySelector('h1').textContent = 'Yay!!!🥳 B-day🎂 Party💕🎉🎊';
       document.querySelector('.message').textContent =
         'Wow!😯 You rememberd my birthday🎂. Thank you!😍';
       document.querySelector('.number').textContent = birthDate;
       document.querySelector('img').src = 'happy_dora.jpg';
-
+      document.querySelector('img').style.width = '90px';
       document.querySelector('body').style.backgroundImage = "url('bg.jpg')";
-    } else if (guess > birthDate) {
+      if (score > highScore) {
+        highScore = score;
+        document.querySelector('.highscore').textContent = score;
+      }
+      //When guess is wrong
+    } else if (guess !== birthDate) {
       document.querySelector('.message').textContent =
-        'I am little older than this!😢';
-      document.querySelector('img').src = 'Dora.jpg';
-      document.querySelector('body').style.backgroundImage =
-        "url('birthdaybg.jpg')";
-      document.querySelector('.score').textContent = --score;
-    } else if (guess < birthDate) {
-      document.querySelector('.message').textContent =
-        'I am little younger than this!😢';
-      document.querySelector('body').style.backgroundImage =
-        "url('birthdaybg.jpg')";
+        guess < birthDate
+          ? 'I am little younger than this!😢'
+          : 'I am little older than this!😢';
 
+      document.querySelector('body').style.backgroundImage =
+        "url('birthdaybg.jpg')";
       document.querySelector('img').src = 'Dora.jpg';
       document.querySelector('.score').textContent = --score;
     }
+    //if you lost
   } else {
     document.querySelector('.message').textContent =
       'You forgot my birthday!🤦‍♀️😭';
+    document.querySelector('img').src = 'sadDora.jpg';
+    document.querySelector('img').style.width = '90px';
     document.querySelector('.score').textContent = 0;
+    document.querySelector('.number').textContent = 'X👎';
+    document.querySelector('h1').textContent =
+      'You are not invited to the party💔😓';
   }
+});
+
+//"Again" - Reset the game
+document.querySelector('.again').addEventListener('click', function () {
+  birthDate = Math.trunc(Math.random() * 31 + 1);
+  score = 20;
+  document.querySelector('h1').textContent = `🤔Guess Dora's Birthday🎂!`;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...!';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('img').src = 'Dora.jpg';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('body').style.backgroundImage =
+    "url('birthdaybg.jpg')";
 });
